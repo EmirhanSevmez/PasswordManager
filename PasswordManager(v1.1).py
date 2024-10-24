11566b39b8425bf5d3a73619ca4944f82aee4c80
Emirhan Sevmez <160799265+EmirhanSevmez@users.noreply.github.com>
2024-10-24 14:38:08 +0300

    Update PasswordManager(v1.1).py

--- PasswordManager(v1.1).py
+++ PasswordManager(v1.1).py
@@ -12,28 +12,28 @@ def move_to_appdata():
 
 def generate_key(): #   Function to generate a key
     key = Fernet.generate_key() # Generating a key
-    with open("key.key", "wb") as file: # Opening the file in write binary mode
+    with open("sec.key", "wb") as file: # Opening the file in write binary mode
         file.write(key) # Writing the key to the file
     
 def encrypt(): # Function to encrypt the password
-    with open("key.key", "rb") as file: # Opening the file in read binary mode
+    with open("sec.key", "rb") as file: # Opening the file in read binary mode
         key = file.read() # Reading the key from the file
     cipher_suite = Fernet(key) # Creating a cipher suite object
-    with open("password.txt", "rb") as file: # Opening the file in read binary mode
+    with open("windows.txt", "rb") as file: # Opening the file in read binary mode
         data = file.read() # Reading the data from the file
     encrypted_data = cipher_suite.encrypt(data) # Encrypting the data
-    with open("password.txt", "wb") as file: # Opening the file in write binary mode
+    with open("windows.txt", "wb") as file: # Opening the file in write binary mode
         file.write(encrypted_data) # Writing the encrypted data to the file
         
     
 def decrypt(): # Function to decrypt the password
-    with open("key.key","rb") as file: # Opening the file in read binary mode
+    with open("sec.key","rb") as file: # Opening the file in read binary mode
         key = file.read() # Reading the key from the file
     cipher_suite = Fernet(key) # Creating a cipher suite object
-    with open("password.txt", "rb") as file: # Opening the file in read binary mode
+    with open("windows.txt", "rb") as file: # Opening the file in read binary mode
         encrypted_data = file.read() # Reading the encrypted data from the file
     decrypted_data = cipher_suite.decrypt(encrypted_data).decode() # Decrypting the data
-    with open("password.txt", "w") as file: # Opening the file in write mode
+    with open("windows.txt", "w") as file: # Opening the file in write mode
         file.write(str(decrypted_data)) # Writing the decrypted data to the file
 
 
@@ -45,7 +45,7 @@ def Add(): # Function to add a new password
         print("Username or Password cannot be empty") # Printing an error message
         return # Returning from the function
     else: # If the username and password are not empty
-        with open("password.txt", "a") as f: # Opening the file in append mode
+        with open("windows.txt", "a") as f: # Opening the file in append mode
             f.write(f"{username} {password}\n") # Writing the username and password to the file
             print("Password Added Successfully") # Printing a success message
 def delete(): # Function to delete a password
@@ -53,19 +53,19 @@ def delete(): # Function to delete a password
     if not username.strip(): # Checking if the username is empty
             print("Username cannot be empty") # Printing an error message
             return # Returning from the function
-    with open("password.txt", "r") as f: # Opening the file in read mode
+    with open("windows.txt", "r") as f: # Opening the file in read mode
         lines = f.readlines() # Reading all the lines from the file
-    with open("password.txt", "w") as f: # Opening the file in write mode
+    with open("windows.txt", "w") as f: # Opening the file in write mode
         for line in lines: # Iterating over each line
             if username not in line: # Checking if the username is not in the line
                 f.write(line) # Writing the line to the file
         print("Password Deleted Successfully") # Printing a success message
 def list(): # Function to list all the passwords
-    with open("password.txt", "r") as f: # Opening the file in read mode
+    with open("windows.txt", "r") as f: # Opening the file in read mode
         print(f.read()) # Reading and printing all the lines from the file
 def show(): # Function to show a password
     username = input("Enter Username: ") # Reading the username from the user
-    with open("password.txt", "r") as f: # Opening the file in read mode
+    with open("windows.txt", "r") as f: # Opening the file in read mode
         lines = f.readlines() # Reading all the lines from the file
     for line in lines: # Iterating over each line
         if not username.strip(): # Checking if the username is empty
@@ -78,32 +78,32 @@ def RandomAdd(): # Function to add a random password
     characters = string.ascii_letters + string.digits + string.punctuation # Defining the characters to be used in the password
     password = ''.join(random.choice(characters) for i in range(10)) # Generating a random password of length 10
     username = input("Enter Username: ") # Reading the username from the user
-    with open("password.txt", "a") as f: # Opening the file in append mode
+    with open("windows.txt", "a") as f: # Opening the file in append mode
         f.write(f"{username} {password}\n") # Writing the username and password to the file
         print("Password Added Successfully") # Printing a success message
  
 
 def CleanSpace():
-    with open("password.txt", "r") as f: # Opening the file in read mode
+    with open("windows.txt", "r") as f: # Opening the file in read mode
         lines = f.readlines() # Reading all the lines from the file
         cleanlines = [line.strip() for line in lines if line.strip()] # Removing any empty lines
-    with open("password.txt", "w") as f: # Opening the file in write mode
+    with open("windows.txt", "w") as f: # Opening the file in write mode
         f.writelines(cleanlines) # Writing the clean lines to the file
         
 move_to_appdata() # Moving the file to the appdata folder
         
 try:
-    with open("key.key", "rb") as key_file: # Opening the key file in read binary mode
+    with open("sec.key", "rb") as key_file: # Opening the key file in read binary mode
         key = key_file.read() # Reading the key from the file
 except FileNotFoundError: # If the key file is not found
     generate_key()
 try:
-    with open("password.txt", "rb") as password_file:# Opening the password file in read binary mode
+    with open("windows.txt", "rb") as password_file:# Opening the password file in read binary mode
         password_file.read() # Reading the password file
     decrypt()
         
 except FileNotFoundError: # If the password file is not found
-    with open("password.txt", "w") as password_file: # Opening the password file in write mode
+    with open("windows.txt", "w") as password_file: # Opening the password file in write mode
         password_file.write("") # Writing an empty string to the file
         
 
